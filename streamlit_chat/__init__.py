a482250f7ca65e10782eb9b9e83e9ef064254b81
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-02-25 22:13:34 +0530

    Update __init__.py

--- streamlit_chat/__init__.py
+++ streamlit_chat/__init__.py
@@ -1,6 +1,11 @@
 import streamlit.components.v1 as components
 import os
 from typing import Literal, Optional, Union
+import streamlit as st
+from streamlit_chat import message
+
+message("My message") 
+message("Hello bot!", is_user=True)  # align's the message to the right
 
 
 _RELEASE = True
